c87fffabda43f7c5a242ef180e6e900e7c244f88
Mateo Molina <robert.mateo.3012@gmail.com>
2026-05-26 01:02:04 -0500

    feat:Postman funcionando, conjunto con los EndPoints

--- apihibernate/src/main/java/com/krakedev/apihibernate/ApihibernateApplication.java
+++ apihibernate/src/main/java/com/krakedev/apihibernate/ApihibernateApplication.java
@@ -2,8 +2,14 @@ package com.krakedev.apihibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.krakedev")
+
+@EnableJpaRepositories(basePackages = "com.krakedev")
+
+@EntityScan(basePackages = "com.krakedev")
 public class ApihibernateApplication {
 
 	public static void main(String[] args) {
